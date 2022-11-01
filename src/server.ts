a284f4fbfb0b3cece3451546b21a8fa472158dab
WilliamWJD <william.jose.wjd@gmail.com>
2022-10-31 22:15:54 -0300

    feat: adicionando cors no projeto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(routes);
 
